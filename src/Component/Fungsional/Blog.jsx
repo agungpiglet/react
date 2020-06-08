05993a55c511911a1d8ab1c7a7c53bc3aca21751
Agung Piglet <agungpiglet@gmail.com>
2020-06-08 16:39:05 +0700

    berhasil menggunakan props

--- src/Component/Fungsional/Blog.jsx
+++ src/Component/Fungsional/Blog.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './CSS/Blog.css';
 
-const Blog = () => {
+const Blog = (props) => {
     return <div className="blog-wrap">
         <img src="https://placeimg.com/640/480/tech" alt=""/>
-        <p>Tanggal Blog</p>
-        <p>Judul Blog</p>
-        <p>Summary Blog</p>
+        <p>{props.tanggal}</p>
+        <p>{props.judul}</p>
+        <p>{props.summary}</p>
     </div>
 }
 
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -3,7 +3,21 @@ import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        <Blog/>
+        <Blog
+            tanggal="08 Juni 2020"
+            judul="Manusia Purba"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
+        <Blog
+            tanggal="09 Juni 2020"
+            judul="Tekhnologi Informasi"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
+        <Blog
+            tanggal="10 Juni 2020"
+            judul="Sistem Informasi"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
     </div>
 }
 
