87658cb05537561b342b9064aab6d59bd7b32867
Agung Piglet <agungpiglet@gmail.com>
2020-06-08 16:27:51 +0700

    sukses membuat blog css

--- src/Component/Fungsional/Blog.jsx
+++ src/Component/Fungsional/Blog.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import './CSS/Blog.css';
 
 const Blog = () => {
-    return <div>
+    return <div className="blog-wrap">
         <img src="https://placeimg.com/640/480/tech" alt=""/>
         <p>Tanggal Blog</p>
         <p>Judul Blog</p>
