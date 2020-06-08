c8fb529783a3c4e35c2e262fc3e446cebc2b3c3e
Agung Piglet <agungpiglet@gmail.com>
2020-06-08 16:16:52 +0700

    Menambahkan Blog di fungsional

---
+++ src/Component/Fungsional/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+    return <div>
+        <img src="https://placeimg.com/640/480/tech" alt=""/>
+        <p>Tanggal Blog</p>
+        <p>Judul Blog</p>
+        <p>Summary Blog</p>
+    </div>
+}
+
+export default Blog;
