ede0fcc975de6b727cd62a0e40e3dfd1fe60d50f
Agung Piglet <agungpiglet@gmail.com>
2020-06-08 16:42:27 +0700

    mengedit judul blog

--- src/Component/Fungsional/Blog.jsx
+++ src/Component/Fungsional/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = (props) => {
     return <div className="blog-wrap">
         <img src="https://placeimg.com/640/480/tech" alt=""/>
         <p>{props.tanggal}</p>
-        <p>{props.judul}</p>
+        <h2>{props.judul}</h2>
         <p>{props.summary}</p>
     </div>
 }
